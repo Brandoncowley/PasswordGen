1e4cd7a7b2606454b9f287185ea219e385e70192
Brandon Cowley <brandon.cowley@gmail.com>
2021-04-11 15:29:37 -0700

    Code is working!

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
+// WRITE FUNCTIONS TO GET ALL CHARACTERS FOR PASSWORD
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var specChar = "#$%&'()*+,-./:;<=>?@[]^_`{|}";
+var numbers = "0123456789";
+var passwordLength = 0;
+var lowerCasePrompt = "";
+var characterLists = [];
+;
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// WRITE FUNCTIONS TO GET ALL CHARACTERS FOR PASSWORD
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-
-function getRandomSymbol() {
-  const symbols = "#$%&'()*+,-./:;<=>?@[]^_`{|}";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
 
 // Write password to the #password input
 function writePassword() {
@@ -32,24 +22,60 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
-  lengthPassword();
+//when button is clicked, "writePassword" is initiated
+  var password = "";
+  var isValidLength = lengthPassword();
+  if (isValidLength) {
 
-function lengthPassword() {
-  passwordLength = window.prompt("Enter the number of characters you would like in your password. Your password must be between 8 and 120 characters");
-  } 
+    var useUpperCase = upperCaseFunction();
+    if (useUpperCase) characterLists.push(upperCase)
+    var useLowerCase = lowerCaseFunction();
+    if (useLowerCase) characterLists.push(lowerCase)
+    var useSpecChar = specCharFunction();
+    if (useSpecChar) characterLists.push(specChar)
+    var useNumbers = numFunction();
+    if (useNumbers) characterLists.push(numbers)
+    for (var i = 0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * characterLists.length)
+      var characterArray = characterLists [randomIndex]
+      var randomIndex2 = Math.floor(Math.random() * characterArray.length)
+      password += characterArray[randomIndex2]
+    }
+  }
+
+return password
+
+function lengthPassword() {//PROMPT is used to generate the dialogue box
+  passwordLength = prompt("Enter the number of characters you would like in your password. Your password must be between 8 and 120 characters");
+  
+  if (!passwordLength) {
+    return false;
+  };
+  if (passwordLength === " " || isNaN(passwordLength) || passwordLength < 8 || passwordLength > 129) {
+    alert("The password length does not meet requirements, please try again");
+    return false;
+  };
+  return true
+} 
+
+//Needto ask if user wants each of the 4 options
+function upperCaseFunction() {
+  var upperCasePrompt = confirm("Would you like to include UPPER CASE characters?")
+  return upperCasePrompt;
+}//the phrase "confirm" is what generates this window
+
+function lowerCaseFunction() {
+  var lowerCasePrompt = confirm("Would you like to include lower case characters?")
+  return lowerCasePrompt;
+} 
+
+function specCharFunction() {
+  var specCharPrompt = confirm("Would you like to include $pe{i@l characters?")
+  return specCharPrompt;
 }
 
-function includeUpperCase() {
-  passwordLength = window.prompt("Would you like to inlude upper case characters?");
-  } 
+function numFunction() {
+  var numberPrompt = confirm("Would you like to include numb3r5?")
+  return numberPrompt;
 
-
-function includeLowerCase() {
-  passwordLength = window.prompt("Would you like to include lower case characters?");
-  }
-
-
-function includeSymbols() {
-  passwordLength = window.prompt("Would you like to include symbols?");
-  }
-
+}}
